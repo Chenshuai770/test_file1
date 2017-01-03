85fc724e22e8feac9481a0e3f32128c56ac8cc63
chenshuai <770456288@qq.com>
2017-01-03 14:46:47 +0800

    [add ] 加入了数据库Litepal框架的详细解释

---
+++ app/src/main/java/cs/com/test_file1/litepal/Category.java
@@ -0,0 +1,27 @@
+package cs.com.test_file1.litepal;
+
+/**
+ * Created by chenshuai on 2017/1/3.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
